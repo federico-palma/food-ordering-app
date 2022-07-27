3f376a9d7203ba590d1fec6071f69e19b023bdd3
federico-palma <fedepal9@gmail.com>
2022-07-27 12:16:41 +0200

    Add header component and styling

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,13 +1,14 @@
 import mealsImage from "../../Assets/meals.jpeg";
 
-import classes from './Header.module.css'
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="Table full of food" />
